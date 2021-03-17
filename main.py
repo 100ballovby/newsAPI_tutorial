e212e8c180c182fa8d1f4f7a4af90abf71cac270
demidraksin <greatraksin@icloud.com>
2021-03-17 10:13:47 +0300

    ☀️Initial commit⭐️

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import requests
+import pandas as pd
+
+KEY = os.environ.get('API_KEY')  # сохраняю ключ для подключения
+
+url = ('http://newsapi.org/v2/everything?'  # search everything
+       'q=xiaomi&'  # news keyword
+       'from=2021-02-17&'  # from date
+       'to=2021-03-17&'  # to date
+       'domains=lenta.ru&'  # domain for search
+       'language=ru&'  # language 
+       'sortBy=popularity&'  # sorting
+       'pageSize=30&'  # number of pages
+       f'apiKey={KEY}')  # api key
+
+response = requests.get(url)
+print(response)
+xiaomi_news = response.json()
+"""with open('xiaomi.json', 'w') as f:
+       f.write(str(xiaomi_news))"""
+
+print(sorted(xiaomi_news.keys()))  # посмотреть ключи ответа сервера
+print(xiaomi_news['articles'])
+
+df = pd.DataFrame(xiaomi_news['articles'])
+print(df)
+
+######
+
+
